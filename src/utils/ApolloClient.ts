a608e1ea727733ee19e5bc44a3bb113cd781cd15
Andres Camilo Ocampo Ramirez <andres_ocampo82211@elpoli.edu.co>
2025-06-17 13:39:55 -0500

    deployment fix

--- src/utils/ApolloClient.ts
+++ src/utils/ApolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:8080/api/graphql',
+    uri: 'https://school-backend-final.onrender.com/api/graphql',
   }),
   cache: new InMemoryCache()
 });
